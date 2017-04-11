0955ec735705153a0761e2f4e57b9a6e59a46421
EllenHu <HuFolder@EllenHudeAir.wv.cc.cmu.edu>
2017-04-11 15:05:46 -0400

    try index

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title> Main Page </title>
+    </head>
+    <h1>This is the main page</h1>
+</html>
+<?php
+echo "These lines are printed using PHP </br>";
+echo "This is the main page";
+?>
