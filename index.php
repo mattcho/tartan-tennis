d6a5a3f2a003e761ca4ecd9fed61bbfc573ff40c
Fan Yuan <fanyuan@Fans-MBP.wv.cc.cmu.edu>
2017-04-11 15:08:11 -0400

    Add an index page

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+     <head>
+        <title> Main Page </title>
+    </head>
+    <h1>This is the main page</h1>
+</html>
+<?php
+echo "These lines are printed using PHP </br>";
+echo "This is the main page";
+?>
+
