86a437d4963fec60191df54df0d8b1b23ac1a382
DongUk Cho <dongukcho@gmail.com>
2017-04-12 17:47:25 -0400

    seed.sql updated

--- seed.sql
+++ seed.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Apr 12, 2017 at 09:16 PM
+-- Generation Time: Apr 12, 2017 at 11:46 PM
 -- Server version: 5.6.28
 -- PHP Version: 5.6.25
 
@@ -13,6 +13,91 @@ SET time_zone = "+00:00";
 --
 -- Database: `tartan-tennis`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `appointments_id` int(10) UNSIGNED NOT NULL,
+  `poster_id` int(10) UNSIGNED NOT NULL,
+  `responder_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `follows`
+--
+
+CREATE TABLE `follows` (
+  `follows_id` int(11) NOT NULL,
+  `follower_id` int(11) NOT NULL,
+  `followee_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friends`
+--
+
+CREATE TABLE `friends` (
+  `friends_id` int(11) NOT NULL,
+  `friender_id` int(11) NOT NULL,
+  `friendee_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `likes_id` int(11) NOT NULL,
+  `bad_like` tinyint(1) NOT NULL,
+  `public_like` tinyint(1) NOT NULL,
+  `private_like` tinyint(1) NOT NULL,
+  `liker_id` int(11) NOT NULL,
+  `likee_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `messages_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `message_body` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`messages_id`, `sender_id`, `receiver_id`, `message_body`, `created_at`) VALUES
+(1, 1, 2, 'This is testing.', '2017-04-12 21:31:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `tags_id` int(11) NOT NULL,
+  `time_windows_id` int(11) NOT NULL,
+  `tag_body` varchar(20) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -44,7 +129,7 @@ INSERT INTO `time_windows` (`time_windows_id`, `begins_date`, `begins_time`, `en
 --
 
 CREATE TABLE `users` (
-  `users_id` int(11) NOT NULL,
+  `users_id` int(11) UNSIGNED NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -64,6 +149,60 @@ INSERT INTO `users` (`users_id`, `first_name`, `last_name`, `email`, `pass`) VAL
 --
 
 --
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`appointments_id`),
+  ADD KEY `poster_id` (`poster_id`),
+  ADD KEY `responder_id` (`responder_id`);
+
+--
+-- Indexes for table `follows`
+--
+ALTER TABLE `follows`
+  ADD PRIMARY KEY (`follows_id`),
+  ADD KEY `follower_id` (`follower_id`),
+  ADD KEY `followee_id` (`followee_id`);
+
+--
+-- Indexes for table `friends`
+--
+ALTER TABLE `friends`
+  ADD PRIMARY KEY (`friends_id`),
+  ADD KEY `friender` (`friender_id`),
+  ADD KEY `friendee` (`friendee_id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`likes_id`),
+  ADD KEY `bad_like` (`bad_like`),
+  ADD KEY `public_like` (`public_like`),
+  ADD KEY `private_like` (`private_like`),
+  ADD KEY `liker_id` (`liker_id`),
+  ADD KEY `likee_id` (`likee_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`messages_id`),
+  ADD KEY `sender_id` (`sender_id`),
+  ADD KEY `receiver_id` (`receiver_id`),
+  ADD KEY `created_at` (`created_at`);
+ALTER TABLE `messages` ADD FULLTEXT KEY `body` (`message_body`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`tags_id`),
+  ADD KEY `time_windows_id` (`time_windows_id`),
+  ADD KEY `created_at` (`created_at`);
+ALTER TABLE `tags` ADD FULLTEXT KEY `body` (`tag_body`);
+
+--
 -- Indexes for table `time_windows`
 --
 ALTER TABLE `time_windows`
@@ -81,6 +220,21 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `appointments_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `follows`
+--
+ALTER TABLE `follows`
+  MODIFY `follows_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `friends`
+--
+ALTER TABLE `friends`
+  MODIFY `friends_id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `time_windows`
 --
 ALTER TABLE `time_windows`
@@ -89,4 +243,4 @@ ALTER TABLE `time_windows`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `users_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
