731bc9949023bcf966b10862441a4eea6a318252
DongUk Cho <dongukcho@gmail.com>
2017-04-13 20:13:07 -0400

    mysqli_connect.php out

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tartan Tennis</title>
+</head>
+<body>
+
+<div id="header">
+	<h1>Tartan Tennis</h1>
+	<h2>Find your tennis partner at CMU!</h2>
+</div>
+
+<div id="navigation">
+	<ul>
+		<li><a href="index.php">Tartan Tennis</a></li>
+		<li><a href="login.php">Log In</a></li>
+	</ul>
+</div>
+
+</body>
+</html>
+
+
+
+
+
+<!-- Our solution: On the landing page, there is going to be a empty input asking when you are available and the level of experience.
+ -->
+
+
+<?php
+
+// saving the begins data and time
+// saving the ends date and time
+// always assume the current user is Matt Cho (users_id = 1)
+
+
+?>
+
+<h3>When are you available?</h3>
+<form action="index.php" method="POST">
+ <p>Begins: <input type="date"><input type="time"></p>
+ <p>Ends: <input type="date"><input type="time"></p>
+  <input type="submit">
+ </form>
+
