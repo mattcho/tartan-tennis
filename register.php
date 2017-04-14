7023ef62c900324a0bc410877a208bc5b8e78633
DongUk Cho <dongukcho@gmail.com>
2017-04-13 21:22:12 -0400

    register.php done

---
+++ register.php
@@ -0,0 +1,87 @@
+<?php
+
+$page_title = 'Register';
+
+include ('includes/header.html');
+
+if (isset($_POST['submitted'])) {
+
+	$errors = array();
+
+	if (empty($_POST['first_name'])) {
+		$errors[] = 'You forgot to enter your first name.<br />';
+	} else {
+		$fn = trim($_POST['first_name']);
+	}
+
+	if (empty($_POST['last_name'])) {
+		$errors[] = 'You forgot to enter your last name.<br />';
+	} else {
+		$ln = trim($_POST['last_name']);
+	}
+
+	if (empty($_POST['tennis_level'])) {
+		$errors[] = 'You forgot to enter your tennis level.<br />';
+	} else {
+		$t = trim($_POST['tennis_level']);
+	}
+
+	if (empty($_POST['email'])) {
+		$errors[] = 'You forgot to enter your email address.<br />';
+	} else {
+		$e = trim($_POST['email']);
+	}
+
+	if (!empty($_POST['pass1'])) {
+		if ($_POST['pass1'] != $_POST['pass2']) {
+			$errors[] = 'Your password did not match the confirmed password.<br />';
+		} else {
+			$p = trim($_POST['pass1']);
+		}
+	} else {
+		$errors[] = 'You forgot to enter your password.<br />';
+	}
+
+	// If everything is OK...
+	if (empty($errors)) {
+		require_once('../mysqli_connect.php');
+
+		$q = "INSERT INTO users (first_name, last_name, tennis_level, email, pass) VALUES ('$fn', '$ln', '$t', '$e', SHA1('$p'))";
+
+		$r = @mysqli_query($dbc, $q);
+
+		if ($r) {
+
+			echo '<h1>Thank you!</h1>
+			<p>Your are now registered. You can log in.</p>';
+		} else {
+			echo '<h1>' . mysqli_error($dbc) . '</h1>';
+		}
+		mysqli_close($dbc);
+
+		include('includes/footer.html');
+		exit();
+	} else {
+		foreach($errors as $msg) {
+			echo $msg;
+		}
+	}
+}
+
+?>
+
+<h1>Register</h1>
+<form action="register.php" method="post">
+<p>First Name: <input type="text" name="first_name" value="<?php if (isset($_POST['first_name'])) echo $_POST['first_name']; ?>" /></p>
+<p>Last Name: <input type="text" name="last_name" value="<?php if (isset($_POST['last_name'])) echo $_POST['last_name']; ?>" /></p>
+<p>Tennis Level: <input type="text" name="tennis_level" value="<?php if (isset($_POST['tennis_level'])) echo $_POST['tennis_level']; ?>" /></p>
+<p>Email Address: <input type="email" name="email" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" /></p>
+<p>Password: <input type="password" name="pass1" /></p>
+<p>Confirm Password: <input type="password" name="pass2" /></p>
+<p><input type="submit" name="submit" value="Resister" /></p>
+<input type="hidden" name="submitted" value="TRUE" />
+</form>
+
+<?php
+include ('includes/footer.html');
+?>
