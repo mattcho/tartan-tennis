d1fa6b7ddc12d1fe266b67f3a76d65985dc3dbc8
EllenHu <HuFolder@EllenHudeMacBook-Air.local>
2017-05-01 09:40:48 -0400

    add notification

--- includes/header.php
+++ includes/header.php
@@ -21,7 +21,7 @@
 
 require('mysqli_connect.php');
 
-$q = "SELECT message_id FROM messages WHERE receiver_id={$_COOKIE['user_id']}";
+$q = "SELECT message_id FROM messages WHERE receiver_id={$_COOKIE['user_id']} AND is_read = 0";
 
 $r = @mysqli_query($dbc, $q);
 
@@ -29,7 +29,12 @@ $num = mysqli_num_rows($r);
 
 if (isset($_COOKIE['user_id']) AND isset($_COOKIE['first_name'])) {
 	echo "<li><a href='index.php'>Hi, " . $_COOKIE['first_name'] . "</a></li>";
-	echo "<li><a href='message.php'>Message ($num)</a></li>";
+	//echo "<li><a href='message.php'>New Message ($num)</a></li>";
+	if($num > 0) {
+		echo "<li><a href='message.php'>New Message ($num)</a></li>";
+	} else {
+		echo "<li><a href='message.php'>Message</a></li>";
+	}
 	echo "<li><a href='logout.php'>Log Out</a></li>";
 } else {
 	echo "<li><a href='register.php'>Sign Up</a></li><li><a href='login.php'>Log In</a></li>";
