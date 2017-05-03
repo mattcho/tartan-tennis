df14c4cf73fed54c08aa9256bc6cefb27cbea309
EllenHu <HuFolder@EllenHudeAir.wv.cc.cmu.edu>
2017-05-03 18:34:07 -0400

    like_list add

--- friends_list.php
+++ friends_list.php
@@ -33,17 +33,20 @@ if(isset($_COOKIE['user_id']))
     </tr>';
     while($row = mysqli_fetch_array($rs)) {
         echo
-        '</tr>
-        <td>' . $row['user_id'] . '</td>
-        <td>' . $row['first_name'] . ' ' . $row['last_name'] . '</td>
-        <td>' . $row['email'] . '</td>
-        <td>' . $row['likenum'] . '			<a class="btn btn-primary btn-sm" href="like.php?likee_id='
-							. $row['user_id'] . '">Like</a></td>
-        <td>'. $row['badlikenum'] . '		<a class="btn btn-primary btn-sm" href="Flag.php?likee_id='
-							. $row['user_id'] . '">Flag</a></td>
+        '<tr>
+        <th>' . $row['user_id'] . '</th>
+        <th>' . $row['first_name'] . ' ' . $row['last_name'] . '</th>
+        <th>' . $row['email'] . '</th>
+        <th>' . $row['likenum'] . '			<a class="btn btn-primary btn-sm" href="like.php?likee_id='
+							. $row['user_id'] . '">Like</a></th>
+        <th>'. $row['badlikenum'] . '		<a class="btn btn-primary btn-sm" href="Flag.php?likee_id='
+							. $row['user_id'] . '">Flag</a></th>
         </tr>';                            
     }
     echo '</table>';
+
+    echo   '<a class="btn btn-primary btn-sm" href="like_list.php?user_id='
+                            . $_COOKIE['user_id'] . '">See Private Like </a> </h3> ';
 }
 else
 {
