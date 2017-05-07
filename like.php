7bd972e7b479c31fb37bf76adf7da294847ed645
EllenHu <HuFolder@EllenHudeAir.wv.cc.cmu.edu>
2017-05-07 14:07:09 -0400

    like flag change

--- like.php
+++ like.php
@@ -5,11 +5,20 @@ include ('includes/header.php');
 $likee = $_GET['likee_id'];
 $liker = $_COOKIE['user_id'];
 
-$q = "INSERT INTO likes (public_like, private_like, liker_id, likee_id) VALUES (0, 1, '$liker', '$likee')";
-$r = mysqli_query($dbc, $q);
+$qd = "SELECT * FROM likes WHERE likee_id = {$likee} AND liker_id = {$liker} AND public_like = 1";
+$rd = mysqli_query($dbc, $qd);
+$row = mysqli_num_rows($rd);
 
-if($r) {
-	echo '<h2>You just successfully like the user!</h2>';
+if($row > 0) {
+	echo '<h2>You have already liked the user!</h2>';
+} else {
+	$q = "INSERT INTO likes (public_like, private_like, liker_id, likee_id) VALUES (0, 1, '$liker', '$likee')";
+	$r = mysqli_query($dbc, $q);
+
+	if($r) {
+		echo '<h2>You just successfully like the user!</h2>';
+	}
+
 }
 
 include('includes/footer.html');
