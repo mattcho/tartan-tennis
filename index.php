4b39ebc98ec4ba4c2bee7640958957ef9a4c46dd
DongUk Cho <dongukcho@gmail.com>
2017-05-09 13:53:42 -0400

    seed.sql updated

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ include ('includes/header.php');
 ?>
 
 <div class="row">
-  <div class="col-md-6">
+  <div class="col-md-8">
 	<?php include ('match.php'); ?>
 	<hr />
 	<?php include ('related_content.php'); ?> 
   </div>
-  <div class="col-md-6">
+  <div class="col-md-4">
   	<?php include ('recommendations.php'); ?>
   	<hr />
   	<?php include ('personal.php'); ?> 
